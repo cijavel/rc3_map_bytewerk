854702bd4f9e2df767f3c7c75410e87f9a67131e
cijavel <lugin@ciala.de>
2020-12-21 23:57:27 +0100

    exitLayer added

--- tsx/mapUtilities.tsx
+++ tsx/mapUtilities.tsx
@@ -1,6 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
  <image source="../owl/mapUtilities.png" width="288" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="exitUrl" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="3">
   <properties>
    <property name="collides" type="bool" value="true"/>
